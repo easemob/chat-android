412261594663962e316b265291c433db26b5a1a9
neil <neil.liu@easemob.com>
2019-12-30 19:18:21 +0800

    NEW: add EaseChatRow 1

---
+++ easeui/src/main/java/com/hyphenate/easeui/interfaces/MessageListItemClickListener.java
@@ -0,0 +1,47 @@
+package com.hyphenate.easeui.interfaces;
+
+import com.hyphenate.chat.EMMessage;
+
+/**
+ * 消息列表中的点击事件
+ */
+public interface MessageListItemClickListener{
+	/**
+	 * there is default handling when bubble is clicked, if you want handle it, return true
+	 * another way is you implement in onBubbleClick() of chat row
+	 * @param message
+	 * @return
+	 */
+	boolean onBubbleClick(EMMessage message);
+
+	/**
+	 * click resend view
+	 * @param message
+	 * @return
+	 */
+	boolean onResendClick(EMMessage message);
+
+	/**
+	 * on long click for bubble
+	 * @param message
+	 */
+	void onBubbleLongClick(EMMessage message);
+
+	/**
+	 * click the user avatar
+	 * @param username
+	 */
+	void onUserAvatarClick(String username);
+
+	/**
+	 * long click for user avatar
+	 * @param username
+	 */
+	void onUserAvatarLongClick(String username);
+
+	/**
+	 * message in sending progress
+	 * @param message
+	 */
+	void onMessageInProgress(EMMessage message);
+}
---
+++ easeui/src/main/java/com/hyphenate/easeui/widget/chatrow/EaseChatRow.java
@@ -0,0 +1,307 @@
+package com.hyphenate.easeui.widget.chatrow;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMMessage;
+import com.hyphenate.chat.EMMessage.Direct;
+import com.hyphenate.easeui.EaseUI;
+import com.hyphenate.easeui.R;
+import com.hyphenate.easeui.domain.EaseAvatarOptions;
+import com.hyphenate.easeui.interfaces.MessageListItemClickListener;
+import com.hyphenate.easeui.model.styles.EaseMessageListItemStyle;
+import com.hyphenate.easeui.utils.EaseUserUtils;
+import com.hyphenate.easeui.widget.EaseImageView;
+import com.hyphenate.util.DateUtils;
+
+import java.util.Date;
+
+public abstract class EaseChatRow extends LinearLayout {
+    public interface EaseChatRowActionCallback {
+        void onResendClick(EMMessage message);
+
+        void onBubbleClick(EMMessage message);
+
+        void onDetachedFromWindow();
+    }
+
+    protected static final String TAG = EaseChatRow.class.getSimpleName();
+
+    protected LayoutInflater inflater;
+    protected Context context;
+    protected BaseAdapter adapter;
+    protected EMMessage message;
+    protected int position;
+
+    protected TextView timeStampView;
+    protected ImageView userAvatarView;
+    protected View bubbleLayout;
+    protected TextView usernickView;
+
+    protected TextView percentageView;
+    protected ProgressBar progressBar;
+    protected ImageView statusView;
+    protected Activity activity;
+
+    protected TextView ackedView;
+    protected TextView deliveredView;
+
+    protected MessageListItemClickListener itemClickListener;
+    protected EaseMessageListItemStyle itemStyle;
+
+    private EaseChatRowActionCallback itemActionCallback;
+
+    public EaseChatRow(Context context, EMMessage message, int position, BaseAdapter adapter) {
+        super(context);
+        this.context = context;
+        this.message = message;
+        this.position = position;
+        this.adapter = adapter;
+        this.activity = (Activity) context;
+        inflater = LayoutInflater.from(context);
+
+        initView();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        itemActionCallback.onDetachedFromWindow();
+        super.onDetachedFromWindow();
+    }
+
+    public void updateView(final EMMessage msg) {
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                onViewUpdate(msg);
+            }
+        });
+    }
+
+    private void initView() {
+        onInflateView();
+        timeStampView = (TextView) findViewById(R.id.timestamp);
+        userAvatarView = (ImageView) findViewById(R.id.iv_userhead);
+        bubbleLayout = findViewById(R.id.bubble);
+        usernickView = (TextView) findViewById(R.id.tv_userid);
+
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+//        statusView = (ImageView) findViewById(R.id.msg_status);
+//        ackedView = (TextView) findViewById(R.id.tv_ack);
+//        deliveredView = (TextView) findViewById(R.id.tv_delivered);
+
+        onFindViewById();
+    }
+
+    /**
+     * set property according message and postion
+     * 
+     * @param message
+     * @param position
+     */
+    public void setUpView(EMMessage message, int position,
+                          MessageListItemClickListener itemClickListener,
+                          EaseChatRowActionCallback itemActionCallback,
+                          EaseMessageListItemStyle itemStyle) {
+        this.message = message;
+        this.position = position;
+        this.itemClickListener = itemClickListener;
+        this.itemActionCallback = itemActionCallback;
+        this.itemStyle = itemStyle;
+
+        setUpBaseView();
+        onSetUpView();
+        setClickListener();
+    }
+
+    private void setUpBaseView() {
+    	// set nickname, avatar and background of bubble
+        TextView timestamp = (TextView) findViewById(R.id.timestamp);
+        if (timestamp != null) {
+            if (position == 0) {
+                timestamp.setText(DateUtils.getTimestampString(new Date(message.getMsgTime())));
+                timestamp.setVisibility(View.VISIBLE);
+            } else {
+            	// show time stamp if interval with last message is > 30 seconds
+                EMMessage prevMessage = (EMMessage) adapter.getItem(position - 1);
+                if (prevMessage != null && DateUtils.isCloseEnough(message.getMsgTime(), prevMessage.getMsgTime())) {
+                    timestamp.setVisibility(View.GONE);
+                } else {
+                    timestamp.setText(DateUtils.getTimestampString(new Date(message.getMsgTime())));
+                    timestamp.setVisibility(View.VISIBLE);
+                }
+            }
+        }
+        if(userAvatarView != null) {
+            //set nickname and avatar
+            if (message.direct() == Direct.SEND) {
+                EaseUserUtils.setUserAvatar(context, EMClient.getInstance().getCurrentUser(), userAvatarView);
+            } else {
+                EaseUserUtils.setUserAvatar(context, message.getFrom(), userAvatarView);
+                EaseUserUtils.setUserNick(message.getFrom(), usernickView);
+            }
+        }
+        if (EMClient.getInstance().getOptions().getRequireDeliveryAck()) {
+            if(deliveredView != null){
+                if (message.isDelivered()) {
+                    deliveredView.setVisibility(View.VISIBLE);
+                } else {
+                    deliveredView.setVisibility(View.INVISIBLE);
+                }
+            }
+        }
+        if (EMClient.getInstance().getOptions().getRequireAck()) {
+            if (ackedView != null) {
+                if (message.isAcked()) {
+                    if (deliveredView != null) {
+                        deliveredView.setVisibility(View.INVISIBLE);
+                    }
+                    ackedView.setVisibility(View.VISIBLE);
+                } else {
+                    ackedView.setVisibility(View.INVISIBLE);
+                }
+            }
+        }
+
+        if (itemStyle != null) {
+            if (userAvatarView != null) {
+                if (itemStyle.isShowAvatar()) {
+                    userAvatarView.setVisibility(View.VISIBLE);
+                    // TODO: 2019/12/30 0030 add options's logic
+                    EaseAvatarOptions avatarOptions = null;
+                    if(avatarOptions != null && userAvatarView instanceof EaseImageView){
+                        EaseImageView avatarView = ((EaseImageView)userAvatarView);
+                        if(avatarOptions.getAvatarShape() != 0)
+                            avatarView.setShapeType(avatarOptions.getAvatarShape());
+                        if(avatarOptions.getAvatarBorderWidth() != 0)
+                            avatarView.setBorderWidth(avatarOptions.getAvatarBorderWidth());
+                        if(avatarOptions.getAvatarBorderColor() != 0)
+                            avatarView.setBorderColor(avatarOptions.getAvatarBorderColor());
+                        if(avatarOptions.getAvatarRadius() != 0)
+                            avatarView.setRadius(avatarOptions.getAvatarRadius());
+                    }
+                } else {
+                    userAvatarView.setVisibility(View.GONE);
+                }
+            }
+            if (usernickView != null) {
+                if (itemStyle.isShowUserNick())
+                    usernickView.setVisibility(View.VISIBLE);
+                else
+                    usernickView.setVisibility(View.GONE);
+            }
+            if (bubbleLayout != null) {
+                if (message.direct() == Direct.SEND) {
+                    if (itemStyle.getMyBubbleBg() != null) {
+                        bubbleLayout.setBackground(itemStyle.getMyBubbleBg());
+                    }
+                } else if (message.direct() == Direct.RECEIVE) {
+                    if (itemStyle.getOtherBubbleBg() != null) {
+                        bubbleLayout.setBackground(itemStyle.getOtherBubbleBg());
+                    }
+                }
+            }
+        }
+
+    }
+
+    private void setClickListener() {
+        if(bubbleLayout != null){
+            bubbleLayout.setOnClickListener(new OnClickListener() {
+    
+                @Override
+                public void onClick(View v) {
+                    if (itemClickListener != null && itemClickListener.onBubbleClick(message)){
+                        return;
+                    }
+                    if (itemActionCallback != null) {
+                        itemActionCallback.onBubbleClick(message);
+                    }
+                }
+            });
+    
+            bubbleLayout.setOnLongClickListener(new OnLongClickListener() {
+    
+                @Override
+                public boolean onLongClick(View v) {
+                    if (itemClickListener != null) {
+                        itemClickListener.onBubbleLongClick(message);
+                    }
+                    return true;
+                }
+            });
+        }
+
+        if (statusView != null) {
+            statusView.setOnClickListener(new OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    if (itemClickListener != null && itemClickListener.onResendClick(message)){
+                        return;
+                    }
+                    if (itemActionCallback != null) {
+                        itemActionCallback.onResendClick(message);
+                    }
+                }
+            });
+        }
+
+        if(userAvatarView != null){
+            userAvatarView.setOnClickListener(new OnClickListener() {
+    
+                @Override
+                public void onClick(View v) {
+                    if (itemClickListener != null) {
+                        if (message.direct() == Direct.SEND) {
+                            itemClickListener.onUserAvatarClick(EMClient.getInstance().getCurrentUser());
+                        } else {
+                            itemClickListener.onUserAvatarClick(message.getFrom());
+                        }
+                    }
+                }
+            });
+            userAvatarView.setOnLongClickListener(new OnLongClickListener() {
+                
+                @Override
+                public boolean onLongClick(View v) {
+                    if(itemClickListener != null){
+                        if (message.direct() == Direct.SEND) {
+                            itemClickListener.onUserAvatarLongClick(EMClient.getInstance().getCurrentUser());
+                        } else {
+                            itemClickListener.onUserAvatarLongClick(message.getFrom());
+                        }
+                        return true;
+                    }
+                    return false;
+                }
+            });
+        }
+    }
+
+    protected abstract void onInflateView();
+
+    /**
+     * find view by id
+     */
+    protected abstract void onFindViewById();
+
+    /**
+     * refresh view when message status change
+     */
+    protected abstract void onViewUpdate(EMMessage msg);
+
+    /**
+     * setup view
+     * 
+     */
+    protected abstract void onSetUpView();
+}
