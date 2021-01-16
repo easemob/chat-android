221d6b6e97439c20a626907e1c44e90a686ab7c6
neil <neil.liu@easemob.com>
2021-01-16 10:58:04 +0800

    update max group users.

--- app/src/main/java/com/hyphenate/easeim/section/group/activity/NewGroupActivity.java
+++ app/src/main/java/com/hyphenate/easeim/section/group/activity/NewGroupActivity.java
@@ -40,7 +40,7 @@ public class NewGroupActivity extends BaseInitActivity implements EaseTitleBar.O
     private SwitchItemView itemSwitchInvite;
     private ArrowItemView itemGroupMembers;
     private int maxUsers = 200;
-    private static final int MAX_GROUP_USERS = 2000;
+    private static final int MAX_GROUP_USERS = 3000;
     private static final int MIN_GROUP_USERS = 3;
     private NewGroupViewModel viewModel;
     private String[] newmembers;
@@ -255,7 +255,7 @@ public class NewGroupActivity extends BaseInitActivity implements EaseTitleBar.O
                             maxUsers = Integer.valueOf(content);
                             if(maxUsers > MAX_GROUP_USERS) {
                                 maxUsers = Integer.valueOf(itemGroupMaxUsers.getTvContent().getText().toString().trim());
-                                showToast("建群最大人数不能超过2000！");
+                                showToast("建群最大人数不能超过3000！");
                                 return;
                             }
                             itemGroupMaxUsers.getTvContent().setText(content);
