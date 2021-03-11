341fae6cc01bc279ba5f86aa7e7b790da261566e
Lijian <727402046@qq.com>
2021-03-11 21:07:13 +0800

    修复音视频通话过程中App退到后台点击App不能显示音视频通话页面的问题

--- app/src/main/java/com/hyphenate/easeim/section/base/BaseInitActivity.java
+++ app/src/main/java/com/hyphenate/easeim/section/base/BaseInitActivity.java
@@ -3,7 +3,16 @@ package com.hyphenate.easeim.section.base;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.hyphenate.easecallkit.EaseCallKit;
+import com.hyphenate.easecallkit.base.EaseCallFloatWindow;
+import com.hyphenate.easecallkit.base.EaseCallType;
+import com.hyphenate.easecallkit.ui.EaseMultipleVideoActivity;
+import com.hyphenate.easecallkit.ui.EaseVideoCallActivity;
+import com.hyphenate.easecallkit.utils.EaseCallState;
+
 import androidx.annotation.Nullable;
+
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 
 public abstract class BaseInitActivity extends BaseActivity {
     @Override
@@ -50,4 +59,21 @@ public abstract class BaseInitActivity extends BaseActivity {
      * init data
      */
     protected void initData() { }
+
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        if(EaseCallKit.getInstance().getCallState() != EaseCallState.CALL_IDEL&& !EaseCallFloatWindow.getInstance(getApplicationContext()).isShowing()){
+            if(EaseCallKit.getInstance().getCallType() == EaseCallType.CONFERENCE_CALL){
+                EaseMultipleVideoActivity callActivity = new EaseMultipleVideoActivity();
+                Intent intent = new Intent(getApplicationContext(), callActivity.getClass()).addFlags(FLAG_ACTIVITY_NEW_TASK);
+                getApplicationContext().startActivity(intent);
+            }else{
+                EaseVideoCallActivity callActivity = new EaseVideoCallActivity();
+                Intent intent = new Intent(getApplicationContext(), callActivity.getClass()).addFlags(FLAG_ACTIVITY_NEW_TASK);
+                getApplicationContext().startActivity(intent);
+            }
+        }
+    }
 }
