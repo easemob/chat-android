46ede0cc7bb430411bf253e39d39e016999608e2
doudou <1550540124@qq.com>
2021-10-15 19:42:49 +0800

    update version to 3.8.7

--- app/jni/Application.mk
+++ app/jni/Application.mk
@@ -1,5 +1,5 @@
 #APP_OPTIM := debug
-APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
+APP_ABI := arm64-v8a armeabi-v7a  x86 x86_64
 #APP_ABI := arm64-v8a
 #APP_STL := stlport_static
 APP_STL := gnustl_static
