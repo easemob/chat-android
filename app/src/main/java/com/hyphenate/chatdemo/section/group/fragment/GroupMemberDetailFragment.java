0ec83cb73e7e45c838b9852ea1557bdee0304a8a
apex-wang <1746807718@qq.com>
2023-03-31 15:23:00 +0800

    fix bug groupMemberAttribute input empty data do not change Highlight

--- app/src/main/java/com/hyphenate/chatdemo/section/group/fragment/GroupMemberDetailFragment.java
+++ app/src/main/java/com/hyphenate/chatdemo/section/group/fragment/GroupMemberDetailFragment.java
@@ -144,10 +144,8 @@ public class GroupMemberDetailFragment extends BaseDialogFragment implements Eas
         String content = etContent.getText().toString();
         if (TextUtils.isEmpty(content)){
             cleanBtn.setVisibility(View.GONE);
-            titleBar.getRightText().setTextColor(getResources().getColor(R.color.em_color_common_text_gray));
         }else{
             cleanBtn.setVisibility(View.VISIBLE);
-            titleBar.getRightText().setTextColor(getResources().getColor(R.color.em_login_color_btn_enable_left));
         }
     }
 }
